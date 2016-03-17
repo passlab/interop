f329e4c270917d4b00b540b4c3230508a5f701ea
Yonghong Yan <yanyh15@gmail.com>
2016-03-17 13:38:47 -0400

    sample microbenchmarks for evaluating omp_quiesce
    and set_wait_policy

--- src/pthread-omp.c
+++ src/pthread-omp.c
@@ -12,14 +12,14 @@ int main(int argc, char * argv[])
      const char *message2 = "pthread 2";
 
     /* Create independent threads each of which will execute function */
-     pthread_create(&thread1, NULL, omp_parallel_foo, (void*) message1);
-     pthread_create(&thread2, NULL, omp_parallel_foo, (void*) message2);
+     pthread_create(&thread1, NULL, omp_parallel_foo, (void*) 1);
+     pthread_create(&thread2, NULL, omp_parallel_foo, (void*) 2);
 
      /* Wait till threads are complete before main continues. Unless we  */
      /* wait we run the risk of executing an exit which will terminate   */
      /* the process and all threads before the threads have completed.   */
 
-     omp_parallel_foo("master thread");
+     omp_parallel_foo((void*)0);
 
      pthread_join(thread1, NULL);
      pthread_join(thread2, NULL);
@@ -27,14 +27,56 @@ int main(int argc, char * argv[])
      exit(0);
 }
 
+void busy_waiting(int second) {
+	sleep(second); /* temporary implem */
+}
+
+#define NUM_ITERATIONS 100000000
+
 void *omp_parallel_foo(void *ptr )
 {
-    char *message = (char *) ptr;
+    int user_thread_id = (int) ptr;
+    int i;
+    for (i=0; i<NUM_ITERATIONS; i++) {
+    	/* busy wait for one second */
+    	busy_waiting(user_thred_id);
 #pragma omp parallel shared(message)
 	{
 		int id = omp_get_thread_num();
-		printf("%s: OMP thread: %d,  \n", message, id);
+		printf("%d OMP thread for user thread %d,  \n", message, id, user_thread_id);
+		/* busy waiting, the whole parallel region should takes 1s */
+		busy_waiting(1);
 	}
-	while(1);
+	omp_set_wait_policy(PASSIVE); /* or active */
+	/* or omp_quiesce() */
+
+    }
 	return NULL;
+}
+
+void *omp_quiesce_overhead(void *ptr )
+{
+    int user_thread_id = (int) ptr;
+    int i;
+    double total = read_timer();
+    for (i=0; i<NUM_ITERATIONS; i++) {
+#pragma omp parallel shared(message) firstprivate(omp_start_cost)
+	{
+		
+	}
+	omp_quiesce();
+     }
+     total = read_timer - total;
+     
+     double parallel_overhead = read_timer();
+     for (i=0; i<NUM_ITERATIONS; i++) {
+#pragma omp parallel shared(message)
+	{
+	}
+     }
+     parallel_overhead = read_timer() - parallel_overhead;
+     
+     double quiesce_startup = total - parallel_overhead; /* this includes both the startup and quiesce */
+     
+     return NULL;
 }
